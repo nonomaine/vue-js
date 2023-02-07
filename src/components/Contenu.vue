90a3a5377b71bee693703fe71510e56e2cae2c5e
nordine <nordine@domey.dev>
2023-02-07 11:39:14 +0100

    ...

---
+++ src/components/Contenu.vue
@@ -0,0 +1,17 @@
+<template>
+    <div> {{ txt }}</div>
+</template>
+
+<script>
+export default{
+    data: function(){
+        return{
+            txt: 'Je suis le contenu'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
